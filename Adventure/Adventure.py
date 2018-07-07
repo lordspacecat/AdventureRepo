6bb867856440b3acb79b26004c3b61278618d0e2
lordspacecat <40924054+lordspacecat@users.noreply.github.com>
2018-07-07 17:07:03 -0600

    Update Adventure.py

--- Adventure/Adventure.py
+++ Adventure/Adventure.py
@@ -2,17 +2,16 @@ import discord
 from discord.ext import commands
 
 class Adventure:
-    """My custom cog that does stuff!"""
+    """A generic name for a generic story."""
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(no_pm=True)
+    @commands.command(no_pm=False)
     async def beginadventure(self):
-        """This does stuff!"""
+        """One path of many begins - quote from someone"""
 
-        #Your code will go here
-        await self.bot.say("user.mention began his adventure.")
+        await self.bot.say("Protag-kun began his adventure. Protag-kun was just invited to a special party with his three friends: the upbeat baseball player Cliche-kun, the quiet and shy Yuri-chan, and the popular but sassy Bitch-sama.")
 
 def setup(bot):
     bot.add_cog(Adventure(bot)) 
