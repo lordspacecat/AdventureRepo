a3cad7bae3d876abc1d26d17b9c67ca9b6a10319
lordspacecat <40924054+lordspacecat@users.noreply.github.com>
2018-07-07 22:32:12 -0600

    Update Adventure.py

--- Adventure/Adventure.py
+++ Adventure/Adventure.py
@@ -15,7 +15,7 @@ class Adventure:
         await self.bot.say("You punched!")
 
     @commands.command(no_pm=False)
-    async def beginadventure(self):
+    async def beginadventure(self, ctx):
         """One path of many begins - quote from someone"""
         
         name = ctx.message.author.name
