db23ffac8761315d58006dac3f3d14f81226b445
lordspacecat <40924054+lordspacecat@users.noreply.github.com>
2018-07-07 21:32:56 -0600

    Update Adventure.py

--- Adventure/Adventure.py
+++ Adventure/Adventure.py
@@ -9,9 +9,8 @@ class Adventure:
         
     
     @commands.command()
-    async def punch(self, role : discord.Role):
+    async def punch(self, role):
         """I will punch anyone! >.<"""
-     role = discord.utils.get(server.roles, id=role_id)
 
         await self.bot.say("You punched!")
 
