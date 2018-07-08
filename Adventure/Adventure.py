af4b68f3b82daf0be1836a67e5bf1fa38fdd0d4a
lordspacecat <40924054+lordspacecat@users.noreply.github.com>
2018-07-07 21:44:25 -0600

    Update Adventure.py

--- Adventure/Adventure.py
+++ Adventure/Adventure.py
@@ -13,7 +13,7 @@ class Adventure:
         """I will punch anyone! >.<"""
         
         answer = await self.bot.wait_for_message(timeout=120)
-        if answer not in discord.Role:
+        if answer not in role:
             await self.bot.say("Timed out, canceling")
     
         await self.bot.say("You punched!")
