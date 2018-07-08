e1ffcc68e168305755edeb4c3e08ea8995019515
lordspacecat <40924054+lordspacecat@users.noreply.github.com>
2018-07-07 21:07:14 -0600

    Update Adventure.py

--- Adventure/Adventure.py
+++ Adventure/Adventure.py
@@ -6,7 +6,26 @@ class Adventure:
 
     def __init__(self, bot):
         self.bot = bot
+    @ccrole.command(name="add", pass_context=True)
+    @checks.mod_or_permissions(administrator=True)
+    async def ccrole_add(self, ctx, command: str):
 
+        
+        await self.bot.say('What roles should it add? (Must be comma separated)\nSay `None` to skip adding roles')
+
+        answer = await self.bot.wait_for_message(timeout=999, author=author)
+        if not answer:
+            await self.bot.say("Timed out, canceling")
+            return
+        arole_list = ["Test Role"]
+        if answer.content.upper() != "NONE":
+            arole_list = answer.content.split(",")
+
+            try:
+                arole_list = [discord.utils.get(server.roles, name=role.strip('Test Role')).id for role in arole_list]
+            except:
+                await self.bot.say("Invalid answer, canceling")
+return
     
     @commands.command()
     async def punch(self, user : discord.Member):
@@ -18,7 +37,7 @@ class Adventure:
     async def beginadventure(self):
         """One path of many begins - quote from someone"""
 
-        await self.bot.say("Protag-kun began his adventure. Protag-kun was just invited to a special party with his three friends: the upbeat baseball player Cliche-kun, the quiet and shy Yuri-chan, and the popular but sassy Bitch-sama.")
+        await self.bot.say("Protag-kun began his adventure. \nIf you are 'Ticketed', you may proceed.")
 
 def setup(bot):
     bot.add_cog(Adventure(bot)) 
