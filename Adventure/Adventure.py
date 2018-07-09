f538933452530da6dac0e191f58e89cb2d516031
lordspacecat <40924054+lordspacecat@users.noreply.github.com>
2018-07-08 21:52:38 -0600

    Update Adventure.py

--- Adventure/Adventure.py
+++ Adventure/Adventure.py
@@ -12,10 +12,10 @@ class Adventure:
     async def beginadventure(self, ctx):
         """Begin your adventure."""
         
+        await self.bot.say("You must make a choice. \nIn order to survive, you must kill person 1 or person 2")
+        
         user = ctx.message.author.name
         answer = await self.bot.wait_for_message(timeout=10)
-            
-        await self.bot.say("" + user + " must make a choice. \nIn order to survive, " + user + " must kill person 1 or person 2")
         
         if not answer:
             await self.bot.say("Time's up, " + user + " . Due to your indecisiveness, both Person 1 and Person 2 shall die.")
