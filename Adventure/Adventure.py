4e2dd39db2e51f2b89be986d08c32b216b0df535
lordspacecat <40924054+lordspacecat@users.noreply.github.com>
2018-07-08 22:12:40 -0600

    Update Adventure.py

--- Adventure/Adventure.py
+++ Adventure/Adventure.py
@@ -4,18 +4,8 @@ from discord.ext import commands
 class Adventure
     """This is a cog"""
     
-    def_init_(self, bot):
+    def __init__(self, bot):
         self.bot = bot
-
-    @commands.command(pass_context=True)
-    async def testadventure(self, ctx):
-        """This is a command."""
-        await self.bot.say('What roles should it add? (Must be comma separated)\nSay `None` to skip adding roles')
-
-        answer = await self.bot.wait_for_message(timeout=120, author=author)
-        if not answer:
-            await self.bot.say("Timed out, canceling")
-return
 
     @commands.command(no_pm=False, pass_context=True)
     async def beginadventure(self, ctx):
