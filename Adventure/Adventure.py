e7947f018269fe77fc705ffe1a105db3b4b19cb5
lordspacecat <40924054+lordspacecat@users.noreply.github.com>
2018-07-08 22:21:37 -0600

    Update Adventure.py

--- Adventure/Adventure.py
+++ Adventure/Adventure.py
@@ -16,7 +16,7 @@ class Adventure:
         user = ctx.message.author.name
         answer = await self.bot.wait_for_message(timeout=10)
         
-        if answer:
+        if not answer:
             await self.bot.say("Time's up, " + user + " . Due to your indecisiveness, both Person 1 and Person 2 shall die.")
         
         
