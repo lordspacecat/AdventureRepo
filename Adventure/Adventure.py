a3ae780589612dcee7191a9d635971f04fa4c5bd
lordspacecat <40924054+lordspacecat@users.noreply.github.com>
2018-07-08 20:53:10 -0600

    Update Adventure.py

--- Adventure/Adventure.py
+++ Adventure/Adventure.py
@@ -13,7 +13,7 @@ class Adventure:
         """One path of many begins - quote from someone"""
         
         name = ctx.message.author.name
-        await self.bot.say("" + name + " has began his journey!")
+        await self.bot.say("" + name + " has began his journey on " + ctx.message.timestamp + "!")
 
 def setup(bot):
     bot.add_cog(Adventure(bot)) 
